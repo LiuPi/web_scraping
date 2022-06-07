08c601b62bf7739834ef2bf994b13e122dfa3639
Liudmila <elekelka@gmail.com>
2022-06-07 18:23:40 +0300

    feature: added votes

--- scrape.py
+++ scrape.py
@@ -4,14 +4,17 @@ from bs4 import BeautifulSoup
 res = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
 links = soup.select('.titlelink')
-votes = soup.select('.score')
+subtext = soup.select('.subtext')
 
-def create_custom_hn(links, votes):
+def create_custom_hn(links, subtext):
     hn = []
     for index, item in enumerate(links):
-        title = links[index].getText()
-        href = links[index].get('href', None)
-        hn.append({'title': title, 'link': href})
+        title = item.getText()
+        href = item.get('href', None)
+        vote = subtext[index].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            hn.append({'title': title, 'link': href, 'votes': points})
     return hn
 
-print(create_custom_hn(links, votes))
+print(create_custom_hn(links, subtext))
