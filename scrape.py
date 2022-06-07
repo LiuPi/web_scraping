874dda44e1a58279cb1cefe55b19ad6ce68c94b8
Liudmila <elekelka@gmail.com>
2022-06-07 15:43:18 +0300

    feature: added method to get link and title

--- scrape.py
+++ scrape.py
@@ -2,4 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 
 res = requests.get('https://news.ycombinator.com/news')
-print(res)
+soup = BeautifulSoup(res.text, 'html.parser')
+links = soup.select('.titlelink')
+votes = soup.select('.score')
+
+def create_custom_hn(links, votes):
+    hn = []
+    for index, item in enumerate(links):
+        title = links[index].getText()
+        href = links[index].get('href', None)
+        hn.append({'title': title, 'link': href})
+    return hn
+
+print(create_custom_hn(links, votes))
